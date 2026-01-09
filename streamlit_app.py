fc89ba06ff6542e77a6b39c8e7a938e090b5e4d8
tasuketeangel <tasukete.angel@gmail.com>
2026-01-09 16:54:34 +0000

    nfs

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,13 +19,15 @@ session = cnx.session()
 #session = get_active_session()
 
 
-my_dataframe = session.table("smoothies.public.fruit_options").select('FRUIT_NAME')
-#st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 ingredients_list = st.multiselect(
     'Choose 0-5 ingr',
     my_dataframe
 )
+st.stop()
 
 
 if ingredients_list:
